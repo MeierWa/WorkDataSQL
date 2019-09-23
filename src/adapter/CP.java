812d4869c83ace9be3a4c3a0907e5b51fbcb794f
MeierWa <710010358@qq.com>
2019-09-23 19:04:02 +0800

    add command interface

---
+++ src/adapter/CP.java
@@ -0,0 +1,8 @@
+package adapter;
+
+public class CP {
+    public static final String pathName_pc="D:\\code\\MindFile\\OT";
+    public static final String pathName_phone="/storage/emulated/0/OT";
+    public static final String fileName="sss.txt";
+
+}
